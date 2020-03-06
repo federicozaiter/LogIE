8a5fec5b5ebb6fa11ab6746c419f283df7da29bf
Federico Zaiter <fzaiter@gmail.com>
2020-03-06 20:28:07 -0300

    Added utils and decorators.

---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+
+
+def file_handling(params):
+    if "templates" in params:
+        if not os.path.exists(params['templates']):
+            raise FileNotFoundError(
+                f"File {params['templates']} doesn't exist. "
+                + "Please provide the log templates path."
+            )
+        templates_directory = os.path.dirname(params['processed_templates'])
+        if not os.path.exists(templates_directory):
+            os.makedirs(templates_directory)
+    else:
+        # Checks if preprocessed templates exist as input
+        if not os.path.exists(params['processed_templates']):
+            raise FileNotFoundError(
+                f"File {params['processed_templates']} doesn't exist. "
+                + "Preprocess target logs first and provide their path."
+            )
+
+
+def print_params(params):
+    print("{:-^80}".format("params"))
+    print("Beginning extraction "
+          + "using the following configuration:\n")
+    for param, value in params.items():
+        print("\t{:>13}: {}".format(param, value))
+    print()
+    print("-" * 80)
