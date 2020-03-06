fc2e20f9f8524839c930572869b6c9f79705395e
Federico Zaiter <fzaiter@gmail.com>
2020-03-06 20:29:40 -0300

    Created main experiment module for triples extraction from templates.

---
+++ extraction.py
@@ -0,0 +1,42 @@
+from .preprocess import registry as preprocess_registry
+from .preprocess.utils import load_processed_templates
+from .utils import (
+    file_handling,
+    print_params,
+)
+from .init_params import init_main_args, parse_main_args
+
+
+def init_args():
+    """Init command line args used for configuration."""
+
+    parser = init_main_args()
+    return parser.parse_args()
+
+
+def parse_args(args):
+    """Parse provided args for runtime configuration."""
+    params = parse_main_args(args)
+    return params
+
+
+def main():
+    # Init params
+    params = parse_args(init_args())
+    print_params(params)
+    file_handling(params)  # TODO: handle the case when the experiment ID already exists - this I think is the only one that matters
+    # Preprocess templates
+    if 'templates' in params:
+        preprocess = preprocess_registry.get_preprocessor(params['templates_type'])
+        preprocess(params)
+    # Load preprocessed templates from file
+    templates = load_processed_templates(params)
+    # Run rules triples extraction
+
+    # Run openie triples extraction
+
+    # Run evaluation
+
+
+if __name__ == "__main__":
+    main()
