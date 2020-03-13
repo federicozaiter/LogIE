100e1bd57a9eaea9cfbed12459c7060a97dc0f4c
Federico Zaiter <fzaiter@gmail.com>
2020-03-12 23:38:03 -0300

    Changed how the output from the rules goes into the openie part to passing the output dict. If the openie part needs a file it will take care of it on its own such as the stanford approach.

--- rules/utils.py
+++ rules/utils.py
@@ -5,13 +5,3 @@ import os
 not_word_digit_pattern = re.compile(r'^[^\w\d]+|[^\w\d]+$')
 def power_strip(string):
     return re.sub(not_word_digit_pattern, '', string)
-
-
-def save_remaining(extractions, output_dir):
-    remaining = os.path.join(output_dir, 'remaining.txt')
-    with open(remaining, 'w') as remaining_f:
-        for idx in extractions:
-            remaining_idx = extractions[idx]
-            remaining_idx = [f'{idx}\t{line}' for line in remaining_idx]
-            remaining_f.writelines('\n'.join((*remaining_idx,'')))
-    return remaining
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from .evaluation import registry as eval_registry
 from .openie import registry as openie_registry
 from .rules import registry as rules_registry
-from .rules.utils import save_remaining
 from .preprocess import registry as preprocess_registry
 from .preprocess.utils import load_processed_templates
 from .utils import (
@@ -29,7 +28,7 @@ def main():
     # Init params
     params = parse_args(init_args())
     print_params(params)
-    file_handling(params)  # TODO: handle the case when the experiment ID already exists - this I think is the only one that matters
+    file_handling(params)
     # Preprocess templates
     if 'templates' in params:
         preprocess = preprocess_registry.get_preprocessor(params['templates_type'])
@@ -39,10 +38,9 @@ def main():
     # Run rules triples extraction
     rules_extractor = rules_registry.get_extractor(params['rules'])
     rule_triples, rule_remaining = rules_extractor(templates)
-    remaining_dir = save_remaining(rule_remaining, params['base_dir'])
     # Run openie triples extraction
     openie_extractor = openie_registry.get_extractor(params['openie'])
-    oie_triples, oie_remaining = openie_extractor(remaining_dir, 'triples.txt')
+    oie_triples, oie_remaining = openie_extractor(rule_remaining, 'triples.txt')
     global_result = combine_extractions(oie_triples, rule_triples)
     # Run evaluation
     ground_truth = load_ground_truth(params['ground_truth'])
