e2602eb8e22acc0ef9243b4cdb56853a7d0f0796
Federico Zaiter <fzaiter@gmail.com>
2020-03-12 23:40:47 -0300

    Updated stanford to use the remaining output from the rules as a dict input instead of a file.

--- openie/stanford.py
+++ openie/stanford.py
@@ -4,12 +4,7 @@ import os
 import pathlib
 from shutil import copyfile
 from ..oie_extraction.extraction import Extraction
-
-
-def text_file_to_list(file_name):
-    with open(file_name) as f:
-        result = list(f)
-    return result
+from .utils import text_file_to_list
 
 
 def parse_triples_reverb_format(lines):
@@ -19,7 +14,11 @@ def parse_triples_reverb_format(lines):
         line = line.strip().split('\t')
         idx = line[1]
         triple = (line[2:5])
-        extraction = Extraction.fromTuple(triple, sentence=line[12])
+        extraction = Extraction.fromTuple(
+            triple,
+            sentence=line[12],
+            confidence=line[11]
+            )
         if idx in result:
             result[idx].append(extraction)
         else:
@@ -48,8 +47,16 @@ def clean_temp_file(file_name):
             f.write(sentence)
 
 
+def save_remaining(remaining, output_file):
+    with open(output_file, 'w') as remaining_f:
+        for idx in remaining:
+            remaining_idx = remaining[idx]
+            remaining_idx = [f'{idx}\t{line}' for line in remaining_idx]
+            remaining_f.writelines('\n'.join((*remaining_idx,'')))
+
+
 @register('stanford')
-def extract_triples(source, output):
+def extract_triples(input_remaining, output):
     config = configparser.ConfigParser()
     config_path = os.path.join(
         os.path.dirname(__file__),
@@ -57,10 +64,15 @@ def extract_triples(source, output):
     )
     config.read(config_path)
     jars_dir = os.path.normpath(config['Stanford']['dir'])
-    # the java app has an issue parsing the file name so it's easier to
-    # create a temporary file and then remove it
+    # the java app uses a file as an input so the remaining input is
+    # saved into a temporary file 
+    temp_source = './temp_remaining.txt'
+    save_remaining(input_remaining, temp_source)
+    # the indexing from the Stanford CoreNLP ReVerb format output uses
+    # the ordinal of the sentence so I clean the original index but keep
+    # it in a separate temporary file
     temp_file_name = './temp_source.txt'
-    copyfile(source, temp_file_name)
+    copyfile(temp_source, temp_file_name)
     # We need to clean it so that Stanford CoreNLP has a clean sentence
     # per line in the input file
     clean_temp_file(temp_file_name)
@@ -77,18 +89,19 @@ def extract_triples(source, output):
     print(command)
     print("Extracting triples with StanfordNLP...")
     code = os.system(command)
+    # removing temporary file
     os.remove(temp_file_name)
     if code == 0:
         # parsing results into dicts
         stanford_triples = text_file_to_list(output)
         stanford_triples = parse_triples_reverb_format(stanford_triples)
-        stanford_remaining = text_file_to_list(source)
+        stanford_remaining = text_file_to_list(temp_source)
         stanford_remaining = parse_remaining_reverb_format(stanford_remaining, stanford_triples)
 
         # obtaining a mapping of each line in the output triples from
         # reverb format to the original source template idx
         # note reverb outputs original line ordinal as the id instead
-        stanford_to_idx = [line.strip().split('\t')[0] for line in text_file_to_list(source)]
+        stanford_to_idx = [line.strip().split('\t')[0] for line in text_file_to_list(temp_source)]
 
         triples_output = {}
         remaining_output = {}
@@ -99,3 +112,5 @@ def extract_triples(source, output):
         return triples_output, remaining_output
     else:
         print("Standford CoreNLP OpenIE FAILED")
+    # removing the temporary file that was created
+    os.remove(temp_source)
