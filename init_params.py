7bd44d2696bbd4383e3a88e430f0c3bd58d84207
Federico Zaiter <fzaiter@gmail.com>
2020-03-13 23:01:26 -0300

    Added PredPatt to the OpenIE approaches.

--- init_params.py
+++ init_params.py
@@ -81,7 +81,7 @@ def init_main_args():
         type=str,
         nargs=1,
         default=["stanford"],
-        choices=["stanford", "openie5", "ollie"],
+        choices=["stanford", "openie5", "ollie", "predpatt"],
         help="OpenIE approach to be used for triple extraction.",
     )
     parser.add_argument(
--- openie/__init__.py
+++ openie/__init__.py
@@ -2,4 +2,5 @@ __all__ = [
     "stanford",
     "openie5",
     "ollie",
+    "predpatt",
 ]
